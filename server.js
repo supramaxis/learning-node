b997cff989fa2e658bd3b7084c6d00a30a1da990
supramaxis <kkssccd@gmail.com>
2022-07-14 16:20:58 -0600

    added option to delete shorturls

--- server.js
+++ server.js
@@ -1,43 +1,67 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const ShortUrl = require('./src/models/shortUrl')
-const path = require('path')
-const app = express()
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const ShortUrl = require("./src/models/shortUrl");
+const { render } = require("ejs");
+const app = express();
 
-app.use(express.static(path.join(__dirname, "/src/public")));
-app.set("views", path.join(__dirname + "/src/views"));
+app.use(morgan('dev'));
+app.use(express.json());
+app.set("views", path.join(__dirname, "/src/views"));
+app.use(express.static(path.join(__dirname,"/src/public")));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
-console.log(__dirname + '/src/public')
+console.log(__dirname + "/src/public");
 
-mongoose.connect('mongodb+srv://spmco:DbKc07010@dburl.spiqqii.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true, useUnifiedTopology: true
-})
+const dbconn = process.env.MONGODB_URI
 
+mongoose.connect('mongodb+srv://spmco:DbKc07010@dburl.spiqqii.mongodb.net/?retryWrites=true&w=majority',
+  
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
-
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-
-app.get('/', async (req, res) => {
-  const shortUrls = await ShortUrl.find()
-  res.render('index', { shortUrls: shortUrls })
-})
-
-app.post('/shortUrls', async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl })
-
-  res.redirect('/')
-})
-
-app.get('/:shortUrl', async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-  if (shortUrl == null) return res.sendStatus(404)
-
-  shortUrl.clicks++
-  shortUrl.save()
-
-  res.redirect(shortUrl.full)
-})
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+app.get("/edit", async (req, res) => {
+  res.render("edit");
+});
 
 
-app.listen(process.env.PORT || 5000);
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  
+  res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+  
+  shortUrl.clicks++;
+  shortUrl.save();
+  
+  res.redirect(shortUrl.full);
+});
+app.get("/delete/:shortUrls", async (req, res) => {
+
+    const { shortUrls } = req.params
+
+  await ShortUrl.findByIdAndDelete(shortUrls, (err, result) => {
+
+    console.log('el link ha sido eliminado')
+
+  })
+
+  res.redirect('/');
+});
+
+const port = process.env.PORT || 4400;
+app.listen(process.env.PORT || 4400);
+console.log(`listening at http://localhost:${port}`);
