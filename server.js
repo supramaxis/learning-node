451b69e362c776513ac35a7415e139b9a19590fe
SupraMaxis <kkssccd@gmail.com>
2022-11-08 21:19:49 -0600

    refactored code

--- server.js
+++ server.js
@@ -1,67 +1,16 @@
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const ShortUrl = require("./src/models/shortUrl");
-const { render } = require("ejs");
-const app = express();
-
-app.use(morgan('dev'));
-app.use(express.json());
-app.set("views", path.join(__dirname, "/src/views"));
-app.use(express.static(path.join(__dirname,"/src/public")));
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false }));
-
-console.log(__dirname + "/src/public");
-
-const dbconn = process.env.MONGODB_URI
-
-mongoose.connect('mongodb+srv://spmco:DbKc07010@dburl.spiqqii.mongodb.net/?retryWrites=true&w=majority',
-  
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
-
-app.get("/", async (req, res) => {
-  const shortUrls = await ShortUrl.find();
-  res.render("index", { shortUrls: shortUrls });
-});
-app.get("/edit", async (req, res) => {
-  res.render("edit");
-});
+const app = require('./app'); // importa el archivo app.js que tiene todas las configuraciones de express
+require('dotenv').config();
+const yup = require('yup')
 
 
-app.post("/shortUrls", async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl });
-  
-  res.redirect("/");
-});
+// const schema = yup.object().shape({
+//   slug: yup.string().trim().matches(/^[\w\-]+$/i),
+//   url: yup.string().trim().url().required(),
+// });
 
-app.get("/:shortUrl", async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
-  if (shortUrl == null) return res.sendStatus(404);
-  
-  shortUrl.clicks++;
-  shortUrl.save();
-  
-  res.redirect(shortUrl.full);
-});
-app.get("/delete/:shortUrls", async (req, res) => {
+const db = require('./config/database');
 
-    const { shortUrls } = req.params
 
-  await ShortUrl.findByIdAndDelete(shortUrls, (err, result) => {
-
-    console.log('el link ha sido eliminado')
-
-  })
-
-  res.redirect('/');
-});
-
-const port = process.env.PORT || 4400;
-app.listen(process.env.PORT || 4400);
-console.log(`listening at http://localhost:${port}`);
+const port = process.env.PORT || 4400; // establece el puerto de la aplicacion
+app.listen(process.env.PORT || 4400); // inicia la aplicacion
+console.log(`listening at http://localhost:${port}`); // muestra en consola el puerto de la aplicacion
